76ed5c280e9fcc598d3bf78110e921cd7e0511e7
yasirmehmood <azmyasir250@gmail.com>
2024-03-22 15:51:49 +0500

    add label

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     {label :"Html", content :"Hyper text Markupp language"},
     {label :"css", content :"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aliquam corrupti facilis quasi quae quia tempore minus? Velit, inventore! Esse, consectetur exercitationem illum animi asperiores aliquid culpa dicta quia ad." },
     {label :"js", content :"programming language"},
+    {label :"node js", content :"run time"},
   ]
   return (
     <div>
